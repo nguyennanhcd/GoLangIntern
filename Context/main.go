782c959fe88ce77df7807b43bb7c81fd8d34733a
nguyennanhcd <anh487303@gmail.com>
2025-11-20 17:41:35 +0700

    chorse: add context content

--- Context/main.go
+++ Context/main.go
@@ -19,20 +19,6 @@ func placeOrderWithoutContext(orderID string) error {
 	return nil // Thành công
 }
 
-func OrderHandler(w http.ResponseWriter, r *http.Request) {
-	orderID := "GO-12345"
-	//
-	err := placeOrderWithoutContext(orderID)
-
-	if err != nil {
-		http.Error(w, "Lỗi xử lý đơn hàng", http.StatusInternalServerError) // 500 Internal Server Error
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Đặt hàng thành công!"))
-}
-
 func OrderHandlerSelect(w http.ResponseWriter, r *http.Request) {
 	orderID := "GO-12345"
 	resultChan := make(chan error, 1)
@@ -58,10 +44,23 @@ func OrderHandlerSelect(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// PlaceOrderContext
+func placeOrderWithContext(ctx context.Context, orderID string) error {
+	log.Printf("Bắt đầu xử lý đơn hàng: %s\n", orderID)
+
+	select {
+	case <-time.After(3 * time.Second):
+		log.Printf("Xử lý đơn hàng %s thành công!\n", orderID)
+		return nil
+	case <-ctx.Done():
+		log.Printf("Huỷ xử lý đơn hàng %s: %v\n", orderID, ctx.Err())
+		return ctx.Err()
+	}
+}
+
 func OrderHandlerWithContext(w http.ResponseWriter, r *http.Request) {
 	orderID := "GO-12345"
 
-	// Tạo context có timeout 2 giây
 	ctx, cancel := context.WithTimeout(r.Context(), 2*time.Second)
 	defer cancel()
 
@@ -77,22 +76,24 @@ func OrderHandlerWithContext(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Đặt hàng thành công!"))
 }
 
-// PlaceOrderContext
-func placeOrderWithContext(ctx context.Context, orderID string) error {
-	log.Printf("Bắt đầu xử lý đơn hàng: %s\n", orderID)
-
-	select {
-	case <-time.After(3 * time.Second): // giả lập xử lý mất 3 giây
-		log.Printf("Xử lý đơn hàng %s thành công!\n", orderID)
-		return nil
-	case <-ctx.Done(): // Nếu context bị huỷ
-		log.Printf("Huỷ xử lý đơn hàng %s: %v\n", orderID, ctx.Err())
-		return ctx.Err()
-	}
-}
-
 func main() {
-	http.HandleFunc("/order", OrderHandlerSelect)
-	log.Println("Server đang chạy tại http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	// http.HandleFunc("/order", OrderHandlerWithContext)
+	// log.Println("Server đang chạy tại http://localhost:8080")
+	// log.Fatal(http.ListenAndServe(":8080", nil))
+
+	// context
+	ctx := context.Background() // root -> http request
+
+	ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
+	defer cancel()
+	orderID := "GO-12345"
+
+	err := placeOrderWithContext(ctx, orderID)
+
+	if err != nil {
+		log.Printf("Xử lý đơn hàng %s thất bại: %v\n", orderID, err)
+		return
+	} else {
+		log.Printf("Đặt hàng %s thành công!\n", orderID)
+	}
 }
